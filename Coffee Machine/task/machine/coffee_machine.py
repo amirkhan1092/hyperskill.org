a7ab1f6f425d5c6aeaa2912afb67e74304ada074
amirkhan1092 <amirkhan1092@gmail.com>
2020-05-14 10:55:52 +0530

    coffee stage 2

---
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -0,0 +1,21 @@
+# # Write your code here
+# k = '''Starting to make a coffee
+# Grinding coffee beans
+# Boiling water
+# Mixing boiled water with crushed coffee beans
+# Pouring coffee into the cup
+# Pouring some milk into the cup
+# Coffee is ready!'''
+# print(k)
+print('Write how many cups of coffee you will need:')
+num = int(input())
+
+water = num * 200
+milk = num * 50
+beans = num * 15
+
+print('For 25 cups of coffee you will need:')
+
+print('%d ml of water'%(water))
+print('%d ml of milk'%(milk))
+print('%d g of coffee beans'%(beans))
